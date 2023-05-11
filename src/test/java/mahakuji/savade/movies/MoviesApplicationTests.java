baf919e461db9f5245976ee2f546849432350664
mbsavade <smahakuji@gmail.com>
2023-05-11 22:44:18 +0530

    This is the initial commit where only the basic import was done like: creating project and connecting the same with the mongoDB server.

---
+++ src/test/java/mahakuji/savade/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package mahakuji.savade.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
